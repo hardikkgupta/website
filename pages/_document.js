f188dc0716e2f3fa4da506209a182b316492e439
electricplayer <40640596+electricplayer@users.noreply.github.com>
2024-09-21 23:28:55 -0500

    More Edits

--- pages/_document.js
+++ pages/_document.js
@@ -7,28 +7,28 @@ export default function Document() {
         <meta charSet="utf-8" />
         <meta
           name="description"
-          content="Maxime Bonhomme - Product-focused Developer"
+          content="Hardik Gupta"
         />
         <meta name="image" content="https://bonhomme.lol/meta-og.png" />
-        <meta itemProp="name" content="Bonhomme" />
+        <meta itemProp="name" content="Hardik" />
         <meta
           itemProp="description"
-          content="Maxime Bonhomme - Product-focused Developer"
+          content="Hardik Gupta"
         />
         <meta itemProp="image" content="https://bonhomme.lol/meta-og.png" />
         <meta name="twitter:card" content="summary" />
-        <meta name="twitter:title" content="Bonhomme - Developer" />
+        <meta name="twitter:title" content="Hardik Gupta" />
         <meta
           name="twitter:description"
-          content="Maxime Bonhomme - Product-focused Developer"
+          content="Hardik Gupta"
         />
         <meta name="twitter:site" content="@pixel_arts" />
         <meta name="twitter:creator" content="@pixel_arts" />
         <meta name="twitter:image:src" content="https://bonhomme.lol/meta-og.png" />
-        <meta name="og:title" content="Bonhomme - Developer" />
+        <meta name="og:title" content="Hardik Gupta" />
         <meta
           name="og:description"
-          content="Maxime Bonhomme - Product-focused Developer"
+          content="Hardik Gupta"
         />
         <meta name="og:image" content="https://bonhomme.lol/meta-og.png" />
         <meta name="og:url" content="https://bonhomme.lol/" />
